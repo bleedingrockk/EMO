1a17720ac4f09b315f567a9d6a00fd6ce8453f50
bleedingrock <sureshsuthar438@gmail.com>
2024-08-01 20:02:45 +0530

    Changes for the day

--- js/main_script.js
+++ js/main_script.js
@@ -1,4 +1,26 @@
-document.getElementById('myButton').addEventListener('click', function() {
-    const messageDiv = document.getElementById('message');
-    messageDiv.textContent = 'Button Clicked!';
+document.getElementById('container3_heading_1').addEventListener('click', function() {
+    var target1 = document.getElementById('container4_sub1_1');
+    target1.style.display = 'block'; 
+    var target2 = document.getElementById('container4_sub1_2');
+    target2.style.display = 'none'; 
+    var target3 = document.getElementById('container4_sub1_3');
+    target3.style.display = 'none'; 
+});
+
+document.getElementById('container3_heading_2').addEventListener('click', function() {
+    var target1 = document.getElementById('container4_sub1_1');
+    target1.style.display = 'none'; 
+    var target2 = document.getElementById('container4_sub1_2');
+    target2.style.display = 'block'; 
+    var target3 = document.getElementById('container4_sub1_3');
+    target3.style.display = 'none'; 
+});
+
+document.getElementById('container3_heading_3').addEventListener('click', function() {
+    var target1 = document.getElementById('container4_sub1_1');
+    target1.style.display = 'none'; 
+    var target2 = document.getElementById('container4_sub1_2');
+    target2.style.display = 'none'; 
+    var target3 = document.getElementById('container4_sub1_3');
+    target3.style.display = 'block'; 
 });
