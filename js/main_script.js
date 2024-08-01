dba51596eea359a5da6aca3742af9c7081ba3e1a
bleedingrock <sureshsuthar438@gmail.com>
2024-08-01 11:29:02 +0530

    add

---
+++ js/main_script.js
@@ -0,0 +1,5 @@
+// JavaScript to handle button click
+document.getElementById('myButton').addEventListener('click', function() {
+    const messageDiv = document.getElementById('message');
+    messageDiv.textContent = 'Button Clicked!';
+});
