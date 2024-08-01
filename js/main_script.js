7b093f805009126b9f91bdf3e15f8fe1cd161b0a
bleedingrock <sureshsuthar438@gmail.com>
2024-08-01 15:24:58 +0530

    MADE SOME PROGRESS

--- js/main_script.js
+++ js/main_script.js
@@ -1,4 +1,3 @@
-// JavaScript to handle button click
 document.getElementById('myButton').addEventListener('click', function() {
     const messageDiv = document.getElementById('message');
     messageDiv.textContent = 'Button Clicked!';
